afff097169624adb02cd0c79f7e860dccd12f2ef
Yuri <creator.and.dev@gmail.com>
2017-08-31 19:00:50 +0300

    //complete two columns team (A and B)

--- app/src/main/java/yuri/kl/com/ua/courtcounter/MainActivity.java
+++ app/src/main/java/yuri/kl/com/ua/courtcounter/MainActivity.java
@@ -18,9 +18,12 @@ public class MainActivity extends AppCompatActivity {
      * */
     int scoreTeamA = 0;
 
+    int scoreTeamB = 0;
+
     /**
      *Display the score on click three
-     * submitThree
+     * submit Three
+     * team A
      * */
     public void addThreeForTeamA(View view) {
         final int POINTS_FOR_THREE_THROW = 3;
@@ -31,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      *Display the score on click two
-     * submitThree
+     * submit Two
+     * team A
      * */
     public void addTwoForTeamA(View view) {
         final int POINTS_FOR_TWO_THROW = 2;
@@ -42,7 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      *Display the score on click free
-     * submitThree
+     * submit Free
+     * team A
      * */
     public void addFreeForTeamA(View view) {
         final int POINTS_FOR_FREE_THROW = 1;
@@ -50,12 +55,56 @@ public class MainActivity extends AppCompatActivity {
 
         displayForTeamA(scoreTeamA);
     }
+//----------
+    /**
+     *Display the score on click three
+     * submit Three
+     * team B
+     * */
+    public void addThreeForTeamB(View view) {
+        final int POINTS_FOR_THREE_THROW = 3;
+        scoreTeamB = scoreTeamB + POINTS_FOR_THREE_THROW;
 
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     *Display the score on click two
+     * submit Two
+     * team B
+     * */
+    public void addTwoForTeamB(View view) {
+        final int POINTS_FOR_TWO_THROW = 2;
+        scoreTeamB = scoreTeamB + POINTS_FOR_TWO_THROW;
+
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     *Display the score on click free
+     * submit Free
+     * team B
+     * */
+    public void addFreeForTeamB(View view) {
+        final int POINTS_FOR_FREE_THROW = 1;
+        scoreTeamB = scoreTeamB + POINTS_FOR_FREE_THROW;
+
+        displayForTeamB(scoreTeamB);
+    }
+//-------
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 }
